628f4c253413aa7b6b9df8e96ead504f43acd9bb
Nguyen Hai Nam <hainamnguyen1512@gmail.com>
2021-08-12 17:05:20 +0700

    ok

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Chào Huyền xinh đẹp!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh quá đẹp trai <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh qua đón công chúa Huyền xinh đẹp đi ăn nhé.',
     btnAccept: 'Okiiiii lun <3',
